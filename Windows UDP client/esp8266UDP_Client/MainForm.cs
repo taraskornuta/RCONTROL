552e5c9927120f566de63288e1618b0b053e7ff0
Leon11t <malui2@yandex.ru>
2015-07-02 00:08:34 +0300

    qwert

--- Windows UDP client/esp8266UDP_Client/MainForm.cs
+++ Windows UDP client/esp8266UDP_Client/MainForm.cs
@@ -18,7 +18,6 @@ namespace RCONTROL
 {
     public partial class MainForm : Form
     {
-        Properties.Settings ps = Properties.Settings.Default;
         private Thread JoystickThread = null;
         public int X, Y, Z, RotZ;
         private string ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8;
@@ -26,11 +25,8 @@ namespace RCONTROL
         public MainForm()
         {
             InitializeComponent();
-<<<<<<< HEAD
-=======
             Settings.Load();
             
->>>>>>> origin/master
         }
 
         [DllImport("kernel32.dll", SetLastError = true)] //Enabling debuging console
@@ -39,7 +35,8 @@ namespace RCONTROL
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            if (ps.Debug_Open_Console==true)
+            Settings.Read();
+            if (Settings.Debug_Open_Console==true)
             {
                 AllocConsole(); //Enabling debuging console
             }
@@ -54,7 +51,7 @@ namespace RCONTROL
 
         private void btn_Open_Click(object sender, System.EventArgs e)
         {
-            if (ps.Joy_Enable==true)
+            if (Settings.Joy_Enable == true)
             {
                 JoystickThread = new Thread(new ThreadStart(this.JoyStatus));
                 JoystickThread.Start();
@@ -72,10 +69,10 @@ namespace RCONTROL
             string defaultIP = "192.168.4.1";
             int defaultPort = 8000;
 
-            if (ps.Debug_Enable == true)
+            if (Settings.Debug_Enable == true)
             {
-                defaultIP = ps.Debug_IP;
-                defaultPort = Convert.ToInt16(ps.Debug_Port);
+                defaultIP = Settings.Debug_IP;
+                defaultPort = Convert.ToInt16(Settings.Debug_Port);
             }
 
             // Указываем адрес отправки сообщения
@@ -112,7 +109,7 @@ namespace RCONTROL
             pBar_CH7.Value = tBar_CH7.Value;
             pBar_CH8.Value = tBar_CH8.Value;
 
-            if (ps.Joy_Enable == true)
+            if (Settings.Joy_Enable == true)
             {
                 tBar_CH1.Value = Convert.ToInt16(1000 + (X * 3.92156862745099));
                 tBar_CH2.Value = Convert.ToInt16(1000 + (Y * 3.92156862745099));
@@ -192,7 +189,7 @@ namespace RCONTROL
         }
         public void JoyStatus()
         {
-            if (ps.Joy_Enable == true)
+            if (Settings.Joy_Enable == true)
             {
                 var directInput = new DirectInput();
 
@@ -266,9 +263,7 @@ namespace RCONTROL
         {
             SettingsForm settings = new SettingsForm(); // Створює форму Settings
             settings.Show(); 
-        }
-
-        
+        }       
     }
 }
 
--- Windows UDP client/esp8266UDP_Client/SettingsForm.cs
+++ Windows UDP client/esp8266UDP_Client/SettingsForm.cs
@@ -13,32 +13,33 @@ namespace RCONTROL
 {
     public partial class SettingsForm : Form
     {
-        Properties.Settings ps = Properties.Settings.Default;
-
         private const string Debug_IP_Default = "127.0.0.1";  //Defaults debug IP and Port
         private const string Debug_Port_Default = "8000";
 
         public SettingsForm()
         {
             InitializeComponent();
+            Settings.Load();
 
         }
         private void SettingsForm_Load(object sender, EventArgs e)
         {
-            chkBox_Open_Consol.Checked = ps.Debug_Open_Console;
-            chkBox_Joy_Enable.Checked = ps.Joy_Enable;
-            chkBox_Debug.Checked = ps.Debug_Enable;
-            txt_Debug_IP.Text = ps.Debug_IP;
-            txt_Debug_Port.Text = ps.Debug_Port;
+            Settings.Read();
+
+            chkBox_Open_Consol.Checked = Settings.Debug_Open_Console;
+            chkBox_Joy_Enable.Checked = Settings.Joy_Enable;
+            chkBox_Debug.Checked = Settings.Debug_Enable;
+            txt_Debug_IP.Text = Settings.Debug_IP;
+            txt_Debug_Port.Text = Settings.Debug_Port;
         }
         private void chkBox_Open_Consol_CheckedChanged(object sender, EventArgs e)
         {
-            ps.Debug_Open_Console = chkBox_Open_Consol.Checked;
+            Settings.Debug_Open_Console = chkBox_Open_Consol.Checked;
         }
 
         private void chkBox_Debug_CheckedChanged(object sender, EventArgs e)
         {
-            ps.Debug_Enable = chkBox_Debug.Checked;
+            Settings.Debug_Enable = chkBox_Debug.Checked;
             if (chkBox_Debug.Checked)//Робить не активними поля вводу тексту
             {
                 txt_Debug_IP.Enabled = true;
@@ -52,7 +53,7 @@ namespace RCONTROL
         }
         private void chkBox_Joy_Enable_CheckedChanged(object sender, EventArgs e)
         {
-            ps.Joy_Enable = chkBox_Joy_Enable.Checked;
+            Settings.Joy_Enable = chkBox_Joy_Enable.Checked;
         }
         private void SettingsForm_FormClosing(object sender, FormClosingEventArgs e)
         {          
@@ -60,7 +61,7 @@ namespace RCONTROL
             {
                 if (txt_Debug_IP.Text != "127.0.0.1")
                 {
-                    ps.Debug_IP = txt_Debug_IP.Text;
+                    Settings.Debug_IP = txt_Debug_IP.Text;
                     if (txt_Debug_IP.Text == "")
                     {
                         MessageBox.Show("Please fill Debug IP box","You forgot!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
@@ -68,12 +69,12 @@ namespace RCONTROL
                 }
                 else if (txt_Debug_IP.Text == "127.0.0.1")
                 {
-                    ps.Debug_IP = Debug_IP_Default;                   
+                    Settings.Debug_IP = Debug_IP_Default;                   
                 }
 
                 if (txt_Debug_Port.Text != "8000")
                 {
-                    ps.Debug_Port = txt_Debug_Port.Text;
+                    Settings.Debug_Port = txt_Debug_Port.Text;
                     if (txt_Debug_Port.Text == "")
                     {
                         MessageBox.Show("Please fill Debug Port box", "You forgot!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
@@ -81,11 +82,11 @@ namespace RCONTROL
                 }
                 else if (txt_Debug_Port.Text == "8000")
                 {
-                    ps.Debug_Port = Debug_Port_Default;
+                    Settings.Debug_Port = Debug_Port_Default;
                 }
             }
 
-            ps.Save();
+            Settings.Save();           
         }
 
        
